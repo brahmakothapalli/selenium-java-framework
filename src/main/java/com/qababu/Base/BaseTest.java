49947555797001b489b759fc69066cdc3455a1a9
qababu <brahma457@gmail.com>
2019-09-04 22:24:29 +0530

    initial commit

---
+++ src/main/java/com/qababu/Base/BaseTest.java
@@ -0,0 +1,32 @@
+package com.qababu.Base;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+public class BaseTest {
+
+    @BeforeSuite
+    public static void configSetup(){
+
+    }
+
+    @BeforeMethod
+    public static void driverInit(){
+
+        
+
+    }
+
+    @AfterMethod
+    public static void driverClose(){
+
+    }
+
+
+    @AfterSuite
+    public static void tearDown(){
+
+    }
+}
---
+++ src/main/java/com/qababu/Base/DriverManager.java
@@ -0,0 +1,42 @@
+package com.qababu.Base;
+
+import org.openqa.selenium.WebDriver;
+
+public class DriverManager {
+
+    private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<>();
+
+    private static String browserType;
+
+    public static void setyBrowserType(String browser){
+        browserType = browser;
+    }
+
+    public static WebDriver getDriver(){
+
+        WebDriver driver = DriverManager.threadDriver.get();
+
+        if(driver == null){
+
+            switch (browserType){
+
+                case "CHROME":
+                    System.out.println("Launching Chrome Browser");
+                    break;
+                case "FIREFOX":
+                    System.out.println("Launching Firefox Browser");
+                    break;
+                 default:
+                     break;
+            }
+        }
+
+        return driver;
+    }
+
+    public static void quitDriver(){
+        DriverManager.getDriver().quit();
+        DriverManager.threadDriver.set(null);
+    }
+
+}
