9eb41a3de3b84e0f1e68d30753ccb64aef4d0af7
qababu <brahma457@gmail.com>
2019-09-08 15:27:30 +0530

    ConfigProperties file object called and Log4j prop is used

--- src/main/java/com/qababu/Base/BaseTest.java
+++ src/main/java/com/qababu/Base/BaseTest.java
@@ -1,15 +1,28 @@
 package com.qababu.Base;
 
+import com.qababu.Utility.FileReader.ConfigFileReader;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public class BaseTest {
 
-    @BeforeSuite
-    public static void configSetup(){
+    private static Properties prop;
 
+    private static final Logger logger = Logger.getLogger(BaseTest.class.getSimpleName());
+
+    @BeforeSuite
+    public static void configSetup() throws IOException {
+
+        prop = ConfigFileReader.getConfigPropObject();
+
+        logger.info("The app url - "+prop.getProperty("appURL"));
 
     }
 
