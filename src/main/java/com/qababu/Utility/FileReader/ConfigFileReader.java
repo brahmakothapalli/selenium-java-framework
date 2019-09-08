a0556a7aab3ab2ee66c265b459548c188f1d6a18
qababu <brahma457@gmail.com>
2019-09-08 15:26:01 +0530

    config properties file added and ConfigFileReader class added

--- src/main/java/com/qababu/Utility/FileReader/ConfigFileReader.java
+++ src/main/java/com/qababu/Utility/FileReader/ConfigFileReader.java
@@ -1,4 +1,26 @@
 package com.qababu.Utility.FileReader;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigFileReader {
+
+    private static Properties prop;
+
+    //Restricting other classes to create object
+    private ConfigFileReader() {
+    }
+
+    //To create single object to Config Properties file through-out the project
+    public static Properties getConfigPropObject() throws IOException {
+        if (prop == null) {
+            prop = new Properties();
+            FileInputStream fis = new FileInputStream(new File(".\\src\\main\\resources\\config.properties"));
+            prop.load(fis);
+            fis.close();
+        }
+        return prop;
+    }
 }
